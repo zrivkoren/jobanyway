8e1f19f3f96d15a2904d1aa49e77e9a1653cc39d
Denis Zavorovsky <zrivkoren@gmail.com>
2023-11-22 19:27:43 +0800

    Добавлена генерации ИИ. Добавлено сохранение файла сопроводительного письма и его открытие в Notepad++. Ссылка на вакансию перенесена в env файл.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from bs4 import BeautifulSoup
 import requests
-import lxml
+from bs4 import BeautifulSoup
+# import lxml
 from fake_useragent import UserAgent
 import os
 from dotenv import load_dotenv
 import re
 from datetime import date
+import main_g4f
+import subprocess
 
 load_dotenv()
 
@@ -34,7 +36,7 @@ class Vacancy:
         content = self.parse_vacancy()
         self.position = content["position"]
         self.company_name = content["company_name"]
-        pass
+        self.company_text = content["company_text"]
 
     @property
     def skills(self):
@@ -58,11 +60,12 @@ class Vacancy:
             content["company_name"] = soup.find("span", class_='vacancy-company-name').find(
                 'span', class_='bloko-header-section-2 bloko-header-section-2_lite'
             ).contents[-1]
-            pass
+
             [self.skills.append(
                 x.text if (x.text.lower() not in WORDS_FOR_REPLACE) else WORDS_FOR_REPLACE[x.text.lower()]
             ) for x in soup.findAll('div', class_='bloko-tag bloko-tag_inline')]
             company_text = soup.find('div', class_='vacancy-section').text
+            content["company_text"] = company_text
             self.check_text_on_skills(company_text)
         except Exception as e:
             print(e)
@@ -91,26 +94,30 @@ class Vacancy:
         }
         return compare
 
-    def create_cover_letter(self, resume):
+    def create_ai_text(self):
+        return main_g4f.get_inf(self.company_text)
 
+    def create_cover_letter(self, resume):
         compare = self.compare_skills(my_resume)
-        matching_skills = ' '.join(compare["matching_skills"])
-        my_remaining_skills = ' '.join(compare["my_remaining_skills"])
-        ready_to_study = ' '.join(compare["ready_to_study"])
-        with open(f"{date.today()}_{self.company_name}.txt", "w") as file:
+        matching_skills = ', '.join(compare["matching_skills"])
+        my_remaining_skills = ', '.join(compare["my_remaining_skills"])
+        ready_to_study = ', '.join(compare["ready_to_study"])
+        with open(f"output_files/{date.today()}_{self.company_name}.txt", "w") as file:
             file.write("Добрый день!\n")
             file.write(f"Меня зовут {resume.name}, пишу по вакансии {self.position}.\n")
-            file.write(f"У меня есть опыт разработки на {matching_skills} \n")
+            file.write(f"У меня есть опыт разработки на {matching_skills}.\n")
             file.write(
-                f"Мои проекты можно посмотреть на гитхабе ( {resume.my_github} ) и в резюме  - {resume.resume_file_url}\n")
-            file.write(f"Готов изучить {ready_to_study}\n")
-            file.write(f"Также знаком с {my_remaining_skills}\n\n")
-            file.write(f"Мне интересна ваша компания, потому что \n")
+                f"Мои проекты можно посмотреть на гитхабе {resume.my_github}\n")
+            file.write(f"Готов изучить {ready_to_study}.\n")
             file.write(
-                f", что подразумевает в ходе работы над реально полезным продуктом рост меня как разработчика .\n")
-            file.write(f"Буду благодарен за любую обратную связь.\n\n")
+                f"Также знаком с {my_remaining_skills} и другими технологиями. Подробнее в резюме {resume.resume_file_url} \n\n")
+            file.write(self.create_ai_text())
+            file.write(f"Готов выполнить тестовое задание.\nБуду благодарен за любую обратную связь.\n\n")
             file.write(f"Мои контакты: тг. @{os.getenv('MY_TELEGRAM')}")
-            pass
+            print("Конец обработки вакансии и резюме")
+        file_path = f"output_files/{date.today()}_{self.company_name}.txt"
+        notepad_path = r'C:\Program Files\Notepad++\notepad++.exe'
+        subprocess.run([notepad_path, file_path], shell=True)
 
 
 class MyResume:
@@ -123,6 +130,5 @@ class MyResume:
 
 if __name__ == '__main__':
     my_resume = MyResume()
-    # vacancy1 = Vacancy(url='https://irkutsk.hh.ru/vacancy/89723032')
-
-    pass
+    vacancy = Vacancy(os.getenv("VACATION_URL"))
+    vacancy.create_cover_letter(my_resume)
