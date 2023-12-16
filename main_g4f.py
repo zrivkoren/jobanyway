73c96fb93be78bc5d5a4fadae9f37ec473da27b8
Denis Zavorovsky <zrivkoren@gmail.com>
2023-12-16 18:55:06 +0800

    Добавлены модели с агрегаторами вакансий. они выведены в отдельный файл. Словари с константами вынесены в отдельный файл.  Создана модель сопроводительного письма, логика по нему вынесена в него из других мест.  Шаблон письма и генерации ИИ текста вынесены в отдельный файл

--- main_g4f.py
+++ main_g4f.py
@@ -1,21 +1,20 @@
 import g4f
-
+from templates import get_text_from_template
 
 g4f.debug.logging = True
 
 
-def get_inf(text):
-    print("Начало генерации текста ИИ")
-    text_value = f"""
-        Есть вакансия, в которой следующий текст описывает компанию, напиши якобы ОТ МОЕГО ИМЕНИ почему я хочу быть разработчиком именно в данной компании. Результат должен быть в 2 - 3 предложения, без пафоса, как будто писал реальный человек. Начни с 'Мне интересна ваша компания потому что ....'. В тексте должно быть что то близкое к 'что подразумевает рост меня как разработчика в ходе работы над реально полезным продуктом.' Предложи два(2) варианта
-Текст: {text}
-        """
+def get_inf(text_input):
+    print("Начало генерации текста ИИ...")
+    text_value = get_text_from_template(text_input)
     try:
         response = g4f.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": text_value}],
         )
         print("Конец генерации текста ИИ")
+        if not response:
+            response = text_value
     except Exception as e:
         print("-Возникла ошибка во время генерации текста-. Текст для ручной генерации сформирован.\n")
         print(e)
@@ -25,5 +24,5 @@ def get_inf(text):
 
 if __name__ == '__main__':
     text = """  """
-
+    print("Текст запуска генерации текста ИИ из файла main_g4f.py")
     print(get_inf(text))
