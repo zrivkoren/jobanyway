3b5c8f899f0dfcaf32cd25e6218aa684e42c4a55
Denis Zaborovsky <zrivkoren@gmail.com>
2024-01-06 22:39:36 +0800

    Сделан пример .env файла. Исправлен гитигнор файл. Подправлен шаблон сопроводительного письма.

--- templates_sample.py
+++ templates_sample.py
@@ -2,17 +2,26 @@ import os
 
 
 def get_letter_from_base_template(content: dict) -> str:
-    result = f"""Привет!\nМеня зовут {os.getenv('RESUME_NAME')}, 
-Ваша вакансия {content['this_vacancy'].position} мне подходит.\n    
-Я бывалый разраб в {content['matching_skills']}.
-Дуб дубом в {content['ready_to_study_skills']}.\n
-Но зато умеют вот это {content['my_remaining_skills']}\n\n
-Моя почта: {os.getenv('MY_EMAIL')}\n Всего доброго, хорошего настроения и здоровья!\n  
+    resume_name = os.getenv('RESUME_NAME')
+    resume_email = os.getenv('MY_EMAIL')
+    vacancy_position = content['this_vacancy'].position
+    matching_skills = content['matching_skills']
+    ready_to_study_skills = content['ready_to_study_skills']
+    remaining_skills = content['my_remaining_skills']
+    created_ai_text = content['created_ai_text']
+    result = f"""Привет!\nМеня зовут {resume_name}, 
+Ваша вакансия {vacancy_position} мне подходит.\n    
+Я бывалый разраб в {matching_skills}.
+Дуб дубом в {ready_to_study_skills}.\n
+Но зато умеют вот это {remaining_skills}\n\n
+Вот такой чудесный текст сгенерировал ИИ:\n
+{created_ai_text}\n\n
+Моя почта: {resume_email}\n Всего доброго, хорошего настроения и здоровья!\n  
     """
     return result
 
 
 def get_text_from_template(text: str) -> str:
     return f"""Напиши ка мне нейросеточка текст на основе вот этого {text}
-Да покречпче! Чтобы прям ууух!
+Да покрепче! Чтобы прям ууух!
     """
